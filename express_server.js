260f408cd2ed81d2a4125f462c68cd0769375f37
Fred <frederique.bordeleau@gmail.com>
2020-01-20 21:28:12 +0000

    Adding page for single url and shorter version of this url

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
 });
