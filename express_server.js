d62cb7288d85300198d4e5d879a816e2cd7d09d2
Fred <frederique.bordeleau@gmail.com>
2020-01-22 14:39:13 +0000

    Added a feature to logout and clear username cooki

--- express_server.js
+++ express_server.js
@@ -35,6 +35,11 @@ app.post("/login", (req, res) => {
   res.redirect("/urls")
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username", req.body.username)
+  res.redirect("/urls")
+})
+
 app.get("/urls", (req, res) => {
   let templateVars = { 
     urls: urlDatabase,
